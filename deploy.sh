162a0f5f30a46a7eaae7ce97c9ccad6480e00394
gzp <gzponline@163.com>
2018-04-14 00:30:46 +0800

    auto deploy and push dev branch

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 # hexo 生成部署到github.io
 # 拉取远程分支的dev代码到 当前分支
 git pull origin dev
-hexo clean 
-hexo d -g
-
-# 将源代码上传到dev分支保存
 
 git add .
 git commit -m "auto deploy and push dev branch"
+# 将源代码上传到dev分支保存
 git push origin dev
+# 部署
+hexo clean 
+hexo d -g
