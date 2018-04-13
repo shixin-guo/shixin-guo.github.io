79fa31c086d018e7af3b04a4ef9fe6069314478b
gzp <gzponline@163.com>
2018-04-14 00:36:56 +0800

    auto deploy and push dev branch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 # hexo 生成部署到github.io
 # 拉取远程分支的dev代码到 当前分支
+
+# 注意本地的master 或者dev分支和远程的master 有差别，以后重新部署是要用origin：dev的 远程分支的dev上面才是源代码远程 master 上是hexo 自动生成的代码
 git pull origin dev
 
 git add .
