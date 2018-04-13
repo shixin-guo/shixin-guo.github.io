b29d69afb074326355ead8afab0058862f1c2025
gzp <gzponline@163.com>
2018-04-13 23:59:02 +0800

    change deploy.sh  git set

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
+#!/bin/bash -e
+# hexo 生成部署到github.io
+拉取远程分支的dev代码到 当前分支
+git pull origin dev
 hexo d -g
+
+# 将源代码上传到dev分支保存
+
 git add .
-git commit -m "修改 添加"
-git push origin master:dev
+git commit -m "auto deploy and push dev branch"
+git push origin dev
